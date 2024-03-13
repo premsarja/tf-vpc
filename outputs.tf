b6353870f58e55bb6434df0ae92a307b9683dae0
premsarja <premsagar@prodot.in>
2024-03-13 12:17:05 +0530

    done

---
+++ outputs.tf
@@ -0,0 +1,23 @@
+output "VPC_ID" {
+  value = module.vpc.VPC_ID
+}
+
+output "VPC_CIDR" {
+  value = module.vpc.VPC_CIDR
+}
+
+output "PUBLIC_SUBNET_CIDR" {
+  value = module.vpc.PUBLIC_SUBNET_CIDR
+}
+
+output "PRIVATE_SUBNET_CIDR" {
+  value = module.vpc.PRIVATE_SUBNET_CIDR
+}
+
+output "DEFAULT_VPC_ID" {
+  value = module.vpc.DEFAULT_VPC_ID
+}
+
+output "DEFAULT_VPC_CIDR" {
+    value = module.vpc.DEFAULT_VPC_CIDR
+}
