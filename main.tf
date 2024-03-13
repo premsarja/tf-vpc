e7c8502d258b15fe9657a0917b3e4b509abe2442
premsarja <premsagar@prodot.in>
2024-03-13 12:15:31 +0530

    done

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
 module "vpc" {
-  source = "./vendor/modules/vpc"
-  VPC_CIDR = var.VPC_CIDR
-  ENV      = var.ENV
-  AZ       =       var.AZ
-  PUBLIC_SUBNET_CIDR = var.PUBLIC_SUBNET_CIDR
-  PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
-  DEFAULT_VPC_ID = var.DEFAULT_VPC_ID
-  DEFAULT_VPC_CIDR = var.DEFAULT_VPC_CIDR
+  source                         = "./vendor/modules/vpc"
+  VPC_CIDR                       = var.VPC_CIDR
+  ENV                            = var.ENV
+  AZ                             =       var.AZ
+  PUBLIC_SUBNET_CIDR             = var.PUBLIC_SUBNET_CIDR
+  PRIVATE_SUBNET_CIDR            = var.PRIVATE_SUBNET_CIDR
+  DEFAULT_VPC_ID                 = var.DEFAULT_VPC_ID
+  DEFAULT_VPC_CIDR               = var.DEFAULT_VPC_CIDR
+  DEFAULT_VPC_RT                 = var.DEFAULT_VPC_RT
 }
