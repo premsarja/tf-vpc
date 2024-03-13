9578074b5b6da30c8e432ee9f76c6de3450daa2d
premsarja <premsagar@prodot.in>
2024-03-13 12:19:29 +0530

    done

--- main.tf
+++ main.tf
@@ -7,5 +7,4 @@ module "vpc" {
   PRIVATE_SUBNET_CIDR            = var.PRIVATE_SUBNET_CIDR
   DEFAULT_VPC_ID                 = var.DEFAULT_VPC_ID
   DEFAULT_VPC_CIDR               = var.DEFAULT_VPC_CIDR
-  DEFAULT_VPC_RT                 = var.DEFAULT_VPC_RT
 }
